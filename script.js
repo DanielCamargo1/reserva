df6c56604e3e206392b862a7f402cfb116950918
Daniel Camargo <danidcp098@gmail.com>
2024-02-08 15:53:55 -0300

    function Disabled

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 const baseUrl = "https://localhost:7039";
-
-//  function Reservar(button) {
-//     button.disabled = true;
-//     button.parentNode.parentNode.classList.add('chale-indisponivel'); // Adiciona classe para indicar que o chalé está indisponível
-// }
+ 
 async function enviarReserva() {
     try {
-        // Obtenha os valores dos campos do formulário HTML
         const nomeUsuario = document.getElementById('Username').value;
         const quantidadeDePessoa = parseInt(document.getElementById('quantidadeDePessoa').value);
         const chale = parseInt(document.getElementById('selecioneChale').value);
-        // Crie um objeto reserva com os dados do formulário
+
         const reserva = {
             nomeUsuario,
             quantidadeDePessoa,
@@ -26,7 +21,12 @@ async function enviarReserva() {
 
         if (res.status === 200 || res.status === 201) {
             const resposta = await res.json();
+            function Reservar(button) {
+                button.disabled = true;
+                button.parentNode.parentNode.classList.add('chale-indisponivel'); // Adiciona classe para indicar que o chalé está indisponível
+            }
             console.log('Reserva enviada com sucesso:', resposta);
+           
         } else {
             console.log('Erro ao enviar reserva:', res.status);
         }
