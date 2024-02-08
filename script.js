dfc2642243587b88cec559b9883ac5b11489e900
Daniel Camargo <danidcp098@gmail.com>
2024-02-08 15:38:44 -0300

    j

--- script.js
+++ script.js
@@ -1,33 +1,36 @@
 const baseUrl = "https://localhost:7039";
 
- const nomeUsuario = document.getElementById("name").value;
- const quantidadeDePessoa = parseInt(document.getElementById("quantidadeDePessoa").value);
- const chales = document.getElementById('selecioneChale');
- const chale = chales.value;
+//  function Reservar(button) {
+//     button.disabled = true;
+//     button.parentNode.parentNode.classList.add('chale-indisponivel'); // Adiciona classe para indicar que o chalé está indisponível
+// }
+async function enviarReserva() {
+    try {
+        // Obtenha os valores dos campos do formulário HTML
+        const nomeUsuario = document.getElementById('Username').value;
+        const quantidadeDePessoa = parseInt(document.getElementById('quantidadeDePessoa').value);
+        const chale = parseInt(document.getElementById('selecioneChale').value);
+        // Crie um objeto reserva com os dados do formulário
+        const reserva = {
+            nomeUsuario,
+            quantidadeDePessoa,
+            chale
+        };
+        const res = await fetch(`${baseUrl}/api/Reserva`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(reserva)
+        });
 
-
-async function enviarReserva(){
-    console.log(chale)
-    try{
-         const res = await fetch(`${baseUrl}/api/Reserva`,{
-             method: 'POST',
-             headers: {
-                 'Content-Type' :'application/json'
-         },
-         body : JSON.stringify(reserva) //converte a reserva para json
-     });
-
-         if(res.status === 200 || res.status === 201){
-             const resposta = await res.json();
-             console.log("postou", resposta)
-             // fazer a validção if( não foi ocupado)...
-             //Else passar os nomes
-         }
-         else{
-             console.log('DEU ERRO', res.status)
-         }
-     } catch(erro){
-         console.log("Deu Erro:", erro)
-     }
+        if (res.status === 200 || res.status === 201) {
+            const resposta = await res.json();
+            console.log('Reserva enviada com sucesso:', resposta);
+        } else {
+            console.log('Erro ao enviar reserva:', res.status);
+        }
+    } catch (erro) {
+        console.log("Deu Erro:", erro)
+    }
 }
- 
