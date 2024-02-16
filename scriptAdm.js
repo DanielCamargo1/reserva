079f468e313bb599a4b9ba23b37ec2057a3225f2
Daniel Camargo <danidcp098@gmail.com>
2024-02-16 11:00:04 -0300

    first table console

--- scriptAdm.js
+++ scriptAdm.js
@@ -24,29 +24,15 @@
          spaceForm.style.display = 'none';
          const spaceMensage = document.getElementById("h1");
          const dados = await getReservas();
-         spaceMensage.textContent = JSON.stringify(dados);
+         if(dados != null){
+             spaceMensage.textContent = JSON.stringify(dados);
+         }else{
+            spaceMensage.textContent = 'Não há reservas'
+         }
+         console.table(dados);
      } else {
          alert("Opsss! Senha ou usuário incorreto, tente novamente!");
      }
-
-     function mostrarReservas(reservas) {
-        const container = document.getElementById('xcontainer');
-        container.innerHTML = ''; // Limpa o conteúdo atual antes de adicionar os novos dados
-    
-        reservas.forEach(function (reserva) {
-            const div = document.createElement('div');
-            div.classList.add('reserva');
-    
-            const nomeUsuario = document.createElement('p');
-            nomeUsuario.textContent = `Nome do Usuário: ${reserva.nomeUsuario}`;
-            const quantidadePessoas = document.createElement('p')
-            quantidadePessoas.textContent = `Quantidade: ${reserva.quantidadePessoas}`
-    
-            div.appendChild(nomeUsuario);
-    
-            container.appendChild(div);
-        });
-    }
     
  }
 
